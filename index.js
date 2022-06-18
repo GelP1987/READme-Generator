c4af1c6418c868f6e446c08806d66efdf44fbd15
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-06-18 14:10:13 -0400

    added function to initialize app

--- index.js
+++ index.js
@@ -147,13 +147,24 @@ validate: licenseInput => {
 
  ]);
 };
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, error => {
+        if (error)
+        console.log(error);
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    questions()
+    inquirer.prompt(questions)
+    .then(answers => {
+        const readmeContent m= generateMarkdown(answers);
 
+        writeToFile('.dist/README.md', readmeContent);
+    })
+.catch(error => {
+    console.log(error);
+});
 }
 
 // Function call to initialize app
